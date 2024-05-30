ecb264f3a832c9fb6b8a3f2ba4a689b6421be784
danh <ptdanh986@gmail.com>
2024-05-30 20:57:08 +0700

    first init

---
+++ demo/src/main/java/com/hutech/demo/model/Course.java
@@ -0,0 +1,10 @@
+package com.hutech.demo.model;
+
+import java.time.LocalDate;
+
+public class Course {
+    private int id;
+    private String lectureName;
+    private String place;
+    private LocalDate startDate;
+}
